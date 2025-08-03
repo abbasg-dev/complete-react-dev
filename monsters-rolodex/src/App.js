90a5e9fbbb8325845af8dadf0c8a3f60566d54ce
aghaith <abbasghayth5@gmail.com>
2025-08-03 17:08:11 +0400

    Renders & Re-renders in React

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -13,9 +13,11 @@ class App extends Component {
     this.state = {
       monsters: [],
     };
+    console.log(1);
   }
 
   componentDidMount() {
+    console.log(3);
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
       .then((users) =>
@@ -32,6 +34,7 @@ class App extends Component {
 
   // a special method that returns the UI (JSX) to display.
   render() {
+    console.log(2);
     return (
       <div className="App">
         {this.state.monsters.map((monster) => {
