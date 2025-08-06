05e7bd4a526eda5c96b82e995550a4f91d78af6a
aghaith <abbasghayth5@gmail.com>
2025-08-06 22:09:09 +0400

    SearchBox Component

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,6 +1,7 @@
 import { Component } from "react";
 
 import CardList from "./components/card-list/card-list.component";
+import SearchBox from "./components/search-box/search-box.component";
 import "./App.css";
 
 // App is the child class (or subclass).
@@ -46,11 +47,10 @@ class App extends Component {
 
     return (
       <div className="App">
-        <input
-          className="search-box"
-          type="search"
-          placeholder="search monsters"
-          onChange={onSearchChange}
+        <SearchBox
+          className={"search-box"}
+          onChangeHandler={onSearchChange}
+          placeholder={"search monsters"}
         />
         <CardList monsters={filteredMonsters} />
       </div>
