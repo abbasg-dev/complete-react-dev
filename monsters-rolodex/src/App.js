d397e59be291e2f2550b13b1d1c72889d72958ce
aghaith <abbasghayth5@gmail.com>
2025-08-12 23:10:44 +0400

    Hooks: useState

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,5 +1,4 @@
-import { Component } from "react";
-
+import { useState } from "react";
 import CardList from "./components/card-list/card-list.component";
 import SearchBox from "./components/search-box/search-box.component";
 import "./App.css";
@@ -7,15 +6,23 @@ import "./App.css";
 // App is the child class (or subclass).
 
 const App = () => {
+  const [searchField, setSearchField] = useState(""); // [value, setValue]
+  console.log(searchField);
+
+  const onSearchChange = (event) => {
+    const searchFieldString = event.target.value.toLocaleLowerCase();
+    setSearchField(searchFieldString);
+  };
+
   return (
     <div className="App">
       <h1 className="app-title">Monsters Rolodex</h1>
-      {/* <SearchBox
-          className={"monsters-search-box"}
-          onChangeHandler={onSearchChange}
-          placeholder={"search monsters"}
-        />
-        <CardList monsters={filteredMonsters} /> */}
+      <SearchBox
+        className={"monsters-search-box"}
+        onChangeHandler={onSearchChange}
+        placeholder={"search monsters"}
+      />
+      {/* <CardList monsters={filteredMonsters} /> */}
     </div>
   );
 };
