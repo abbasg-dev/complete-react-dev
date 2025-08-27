869cf1f717923817a505a1504027f5d084c5fa26
aghaith <abbasghayth5@gmail.com>
2025-08-27 16:21:17 +0400

    Products Context

--- crwn-clothing/src/index.js
+++ crwn-clothing/src/index.js
@@ -2,15 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { UserProvider } from "./context/user.context";
+import { ProductsProvider } from "./context/products.context";
 import App from "./App";
 import "./index.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <App />
+        <ProductsProvider>
+          <App />
+        </ProductsProvider>
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
