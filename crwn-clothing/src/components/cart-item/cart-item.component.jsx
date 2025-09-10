f2f8510bf50137dac257574ed993a00498c9efbb
aghaith <abbasghayth5@gmail.com>
2025-09-10 18:24:25 +0400

    Rename imageurl prop as in firebase

--- crwn-clothing/src/components/cart-item/cart-item.component.jsx
+++ crwn-clothing/src/components/cart-item/cart-item.component.jsx
@@ -1,10 +1,10 @@
 import "./cart-item.styles.scss";
 
 const CartItem = ({ cartItem }) => {
-  const { name, imageurl, price, quantity } = cartItem;
+  const { name, imageUrl, price, quantity } = cartItem;
   return (
     <div className="cart-item-container">
-      <img src={imageurl} alt={`${name}`} />
+      <img src={imageUrl} alt={`${name}`} />
       <div className="item-details">
         <span className="name">{name}</span>
         <span className="price">
--- crwn-clothing/src/components/directory-item/directory-item.component.jsx
+++ crwn-clothing/src/components/directory-item/directory-item.component.jsx
@@ -7,14 +7,14 @@ import {
 } from "./directory-item.styles";
 
 const DirectoryItem = ({ category }) => {
-  const { imageurl, title, route } = category;
+  const { imageUrl, title, route } = category;
   const navigate = useNavigate();
 
   const onNavigateHandler = () => navigate(route);
 
   return (
     <DirectoryItemContainer onClick={onNavigateHandler}>
-      <BackgroundImage imageurl={imageurl} />
+      <BackgroundImage imageUrl={imageUrl} />
       <Body>
         <h2>{title}</h2>
         <p>Shop Now</p>
