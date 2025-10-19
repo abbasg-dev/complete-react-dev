b01b2d6bb1fe7ffa413e4cc258bafc59f0a37d5a
aghaith <abbasghayth5@gmail.com>
2025-10-19 22:22:57 +0400

    Categories Selectors

--- crwn-clothing/src/index.js
+++ crwn-clothing/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import App from "./App";
-import { CategoriesProvider } from "./context/categories.context";
 import { CartProvider } from "./context/cart.context";
 import { store } from "./store/store";
 import "./index.scss";
@@ -14,11 +13,9 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <CategoriesProvider>
-          <CartProvider>
-            <App />
-          </CartProvider>
-        </CategoriesProvider>
+        <CartProvider>
+          <App />
+        </CartProvider>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>
