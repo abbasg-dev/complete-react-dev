e9e4c8f6c8e1cc1d0c80ebf975ad5c3e301927c0
aghaith <abbasghayth5@gmail.com>
2025-11-05 23:19:39 +0400

    Refactor Categories state management code

--- crwn-clothing/src/routes/category/category.component.jsx
+++ crwn-clothing/src/routes/category/category.component.jsx
@@ -3,28 +3,26 @@ import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import ProductCard from "../../components/product-card/product-card.component";
 import { selectCategoriesMap } from "../../store/categories/category.selector";
-import "./category.styles.scss";
+import { CategoryContainer, Title } from "./category.styles";
 
 const Category = () => {
   const { category } = useParams();
-  console.log("render/re-rendering category component");
   const categoriesMap = useSelector(selectCategoriesMap);
   const [products, setProducts] = useState(categoriesMap[category]);
 
   useEffect(() => {
-    console.log("effect fired calling setProducts");
     setProducts(categoriesMap[category]);
   }, [category, categoriesMap]);
 
   return (
     <Fragment>
-      <h2 className="category-title">{category.toUpperCase()}</h2>
-      <div className="category-container">
+      <Title>{category.toUpperCase()}</Title>
+      <CategoryContainer>
         {products &&
           products.map((product) => (
             <ProductCard key={product.id} product={product} />
           ))}
-      </div>
+      </CategoryContainer>
     </Fragment>
   );
 };
