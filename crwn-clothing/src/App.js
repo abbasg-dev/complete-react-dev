86f5d9884fe1077488cdb9741216d320859e830a
aghaith <abbasghayth5@gmail.com>
2025-12-02 19:51:42 +0400

    Converting onAuthStateChanged Listener to Promise

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -10,20 +10,15 @@ import { setCurrentUser } from "./store/user/user.action";
 import {
   onAuthStateChangedListener,
   createUserDocumentFromAuth,
+  getCurrentUser,
 } from "./utils/firebase/firebase.utils";
 
 const App = () => {
   const dispatch = useDispatch();
-  useEffect(() => {
-    const unsubscribe = onAuthStateChangedListener((user) => {
-      if (user) {
-        createUserDocumentFromAuth(user);
-      }
-      dispatch(setCurrentUser(user));
-    });
 
-    return unsubscribe;
-  }, [dispatch]);
+  useEffect(() => {
+    getCurrentUser().then((user) => console.log(user));
+  }, []);
 
   return (
     <Routes>
