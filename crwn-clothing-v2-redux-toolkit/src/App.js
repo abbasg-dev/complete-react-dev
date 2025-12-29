17dc1d7bffc36b9874e2eb956ddfd86890e5922d
aghaith <abbasghayth5@gmail.com>
2025-12-29 18:48:21 +0400

    Cart to createSlice

--- crwn-clothing-v2-redux-toolkit/src/App.js
+++ crwn-clothing-v2-redux-toolkit/src/App.js
@@ -22,6 +22,7 @@ const App = () => {
       }
       const pickedUser =
         user && (({ accessToken, email }) => ({ accessToken, email }))(user);
+
       console.log(setCurrentUser(pickedUser));
       dispatch(setCurrentUser(pickedUser));
     });
