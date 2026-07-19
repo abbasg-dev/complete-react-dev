51fc2f37de786bed2f29509ad353c1b9fc899f40
aghaith <abbasghayth5@gmail.com>
2026-07-19 14:12:30 +0400

    Updating Components To Consume GraphQL Values

---
+++ crwn-clothing-v2-graphql/src/contexts/categories.context.jsx
@@ -0,0 +1,47 @@
+import { createContext, useState, useEffect } from "react";
+import { gql } from "@apollo/client";
+import { useQuery } from "@apollo/client/react";
+
+export const CategoriesContext = createContext({
+  categoriesMap: {},
+});
+
+const COLLECTIONS = gql`
+  query {
+    collections {
+      id
+      title
+      items {
+        id
+        name
+        price
+        imageUrl
+      }
+    }
+  }
+`;
+
+export const CategoriesProvider = ({ children }) => {
+  const { loading, error, data } = useQuery(COLLECTIONS);
+  const [categoriesMap, setCategoriesMap] = useState({});
+
+  useEffect(() => {
+    if (data) {
+      const { collections } = data;
+      const collectionsMap = collections.reduce((acc, collection) => {
+        const { title, items } = collection;
+        acc[title.toLowerCase()] = items;
+        return acc;
+      }, {});
+
+      setCategoriesMap(collectionsMap);
+    }
+  }, [data]);
+
+  const value = { categoriesMap, loading };
+  return (
+    <CategoriesContext.Provider value={value}>
+      {children}
+    </CategoriesContext.Provider>
+  );
+};
